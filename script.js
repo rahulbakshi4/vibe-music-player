184e9fbc8d972032f1e39f26a7d2ffbdba203136
Rahul Bakshi <rahulbakshi3@gmail.com>
2020-10-05 02:19:34 +0530

    added more songs and functionality

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
-const nextprevBtn = document.getElementById('next');
- 
+const nextBtn = document.getElementById('next');
+
+// Songs
+const songs = [
+{
+  name: 'song-1',
+  displayname: '1k',
+  artist: 'ameba.'
+},
+{
+  name: 'song-2',
+  displayname: 'memories (w_ aidan)',
+  artist: 'future nat'  
+},
+{
+  name: 'song-3',
+  displayname: 'birds',
+  artist: 'wünsche' 
+}
+] 
+
+
 let isPlaying = false;
 
 function playSong(){  
@@ -20,3 +43,33 @@ function pauseSong(){
 }
 
 playBtn.addEventListener('click', ()=> (isPlaying? pauseSong(): playSong()));
+
+function loadSong(song) {
+    title.textContent = song.displayname;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+let songIndex = 0;
+
+function nextSong() {
+    songIndex = (songIndex+1) % songs.length;
+    console.log(songIndex);
+    loadSong(songs[songIndex]);
+    playSong();
+  }
+   
+  // Prev Song
+  function prevSong() {
+    songIndex = (songIndex-1 + songs.length) % songs.length;
+    loadSong(songs[songIndex]);
+    playSong();
+  }
+
+loadSong(songs[songIndex]);
+
+
+//Event Listeners for buttons
+prevBtn.addEventListener('click',prevSong);
+nextBtn.addEventListener('click',nextSong);
